7655dc46e56109293e7a494c7fedc84062e1a18d
panze <1840521596@qq.com>
2018-09-10 17:11:45 +0800

    "修改注册成功校验"

--- testcase/register_test.py
+++ testcase/register_test.py
@@ -18,16 +18,16 @@ class Test1UserRegister(myunit.MyTest, Register):
         """测试存在用户注册失败"""
         phone = self.cf.get('register_input', 'user')
         password = self.cf.get('register_input', 'pwd')
-        message = self.find_elem_text(self.show_error_loc)
         self.user_register(phone, password)
+        message = self.find_elem_text(self.show_error_loc)
         self.assertEqual('手机号已经注册，请更换其他手机号', message)
 
     def test_user_register_success(self):
         """测试用户注册成功"""
         phone = self.cf.get('unregister_input', 'user')
         password = self.cf.get('unregister_input', 'pwd')
-        message = self.find_elem_text(self.bqj_create_success_loc)
         self.user_register(phone, password)
+        message = self.find_elem_text(self.bqj_create_success_loc)
         self.assertEqual('恭喜账号创建成功！', message)
 
 
