0f627ef1df9cdc9f887332d350f8a72edd07e60a
panze <1840521596@qq.com>
2018-09-12 17:43:59 +0800

    "modify content for exe_caselist_test.py"

--- allcaselist/exe_caselist_test.py
+++ allcaselist/exe_caselist_test.py
@@ -4,6 +4,7 @@ from testcase.verify_real_name_test import Test2VerifyRealName
 from testcase.copyright_credit_test import Test3CreateCopyrightCredit
 from testcase.service_login_test import Test4ServiceLogin
 from testcase.service_quit_test import Test5ServiceQuit
+from testcase.certificate_verify_test import Test6CertificateVerify
 
 
 def test_all_cases_list():
@@ -14,5 +15,7 @@ def test_all_cases_list():
     Test4ServiceLogin().test_login_success()
     Test4ServiceLogin().test_main_nav_jump()
     Test5ServiceQuit().test_quit_success()
-
-
+    Test6CertificateVerify().user_certificate_verify()
+    Test6CertificateVerify().user_trusted_time_verify()
+    Test6CertificateVerify().user_digital_signature_verify()
+    Test6CertificateVerify().user_copyright_block_chain_verify()
