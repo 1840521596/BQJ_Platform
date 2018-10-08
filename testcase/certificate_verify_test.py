494c1114e43ca6aba370778ace882b4c816d9de9
panze <1840521596@qq.com>
2018-10-08 16:41:25 +0800

    "modify certificate_verify_test.py"

--- testcase/certificate_verify_test.py
+++ testcase/certificate_verify_test.py
@@ -17,30 +17,34 @@ class Test4CertificateVerify(myunit.MyTest, methods.CommonMethod, Login, Certifi
     user1 = cf.get("unregister_input", "user")
     pwd1 = cf.get("unregister_input", "pwd")
 
-    # def test_vip_certificate_content_verify(self):
-    #     """测试VIP证书内容核验"""
-    #     self.verify_copyright_pass()
-    #     self.user_login(self.user, self.pwd)
-    #     self.user_certificate_verify()
-    #     self.assertEqual(self.cdData[0], self.sha256())
-    #
-    # def test_vip_trusted_time_verify(self):
-    #     """测试VIP可信时间凭证核验"""
-    #     self.user_login(self.user, self.pwd)
-    #     self.user_trusted_time_verify()
-    #     self.assertIn(self.get_certificate_id(), self.get_trusted_time_verify_result())
-    #
-    # def test_vip_digital_signature_verify(self):
-    #     """测试VIP用户数字签名核验"""
-    #     self.user_login(self.user, self.pwd)
-    #     self.user_digital_signature_verify()
-    #     self.assertEqual('1.本次签名有效', self.get_signature_result())
-    #
-    # def test_vip_copyright_block_chain_verify(self):
-    #     """测试VIP版权区块链联盟核验"""
-    #     self.user_login(self.user, self.pwd)
-    #     self.user_copyright_block_chain_verify()
-    #     self.assertEqual(self.get_certificate_value(), self.get_hash_value())
+    @unittest.skip('跳过VIP证书内容核验')
+    def test_vip_certificate_content_verify(self):
+        """测试VIP证书内容核验"""
+        self.verify_copyright_pass()
+        self.user_login(self.user, self.pwd)
+        self.user_certificate_verify()
+        self.assertEqual(self.cdData[0], self.sha256())
+
+    @unittest.skip('跳过VIP可信时间凭证核验')
+    def test_vip_trusted_time_verify(self):
+        """测试VIP可信时间凭证核验"""
+        self.user_login(self.user, self.pwd)
+        self.user_trusted_time_verify()
+        self.assertIn(self.get_certificate_id(), self.get_trusted_time_verify_result())
+
+    @unittest.skip('跳过VIP用户数字签名核验')
+    def test_vip_digital_signature_verify(self):
+        """测试VIP用户数字签名核验"""
+        self.user_login(self.user, self.pwd)
+        self.user_digital_signature_verify()
+        self.assertEqual('1.本次签名有效', self.get_signature_result())
+
+    @unittest.skip('跳过VIP版权区块链联盟核验')
+    def test_vip_copyright_block_chain_verify(self):
+        """测试VIP版权区块链联盟核验"""
+        self.user_login(self.user, self.pwd)
+        self.user_copyright_block_chain_verify()
+        self.assertEqual(self.get_certificate_value(), self.get_hash_value())
 
     def test_certificate_content_verify(self):
         """测试证书内容核验"""
